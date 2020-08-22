f9058729dcd7e34120cd4d96bd7be2812a03430a
Aseem Aggarwal <aseemaggarwal2009@gmail.com>
2020-08-22 15:28:32 +0530

    Implement Generic Apporach

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Specification;
 
 namespace API.Controllers
 {
@@ -13,17 +14,24 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository _repo;
-        public ProductsController(IProductRepository repo)
+        private readonly IGenericRepository<Product> _productsRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+        public ProductsController(IGenericRepository<Product> productsRepo, 
+        IGenericRepository<ProductType> productTypeRepo, 
+        IGenericRepository<ProductBrand> productBrandRepo)
         {
-            _repo = repo;
-
+            _productBrandRepo = productBrandRepo;
+            _productTypeRepo = productTypeRepo;
+            _productsRepo = productsRepo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _repo.GetProductsAsync();
+            var spec = new ProductsWithTypesAndBrandsSpecification();
+
+            var products = await _productsRepo.ListAsync(spec);
 
             return Ok(products);
         }
@@ -31,21 +39,22 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
-            return await _repo.GetProductByIdAsync(id);
+            return await _productsRepo.GetEntityWithSpec(spec);
         }
 
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
-            var brands = await _repo.GetProductBrandsAsync();
+            var brands = await _productBrandRepo.ListAllAsync();
             return Ok(brands);
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
-            var types = await _repo.GetProductTypesAsync();
+            var types = await _productTypeRepo.ListAllAsync();
             return Ok(types);
         }
     }
---
+++ Core/Specification/ISpecification.cs
@@ -0,0 +1,12 @@
+using System.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+
+namespace Core.Specification
+{
+    public interface ISpecification<T>
+    {
+         Expression<Func<T, bool>> Criteria { get; }
+         List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
